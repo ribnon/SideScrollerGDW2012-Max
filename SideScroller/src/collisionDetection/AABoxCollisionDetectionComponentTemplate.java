597d0ea166baa144c76eb353d1a5e4537ded1a8f
El Fapo <badrandolf90000@web.de>
2012-07-24 16:06:53 +0200

    ComponentTemplates for Collision Detection nearly completed

---
+++ SideScroller/src/collisionDetection/AABoxCollisionDetectionComponentTemplate.java
@@ -0,0 +1,23 @@
+package collisionDetection;
+
+import java.util.HashMap;
+
+import gdw.entityCore.Component;
+import gdw.entityCore.ComponentTemplate;
+
+public class AABoxCollisionDetectionComponentTemplate extends ComponentTemplate
+{
+	
+	protected AABoxCollisionDetectionComponentTemplate(HashMap<String, String> params)
+	{
+		super(params);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Component createComponent()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
