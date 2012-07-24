789d7fa60449d4a3f1c9029e4adc7723edeed7ae
Michael Kulikow <michael.kulikow@gmail.com>
2012-07-24 16:47:55 +0200

    Konstruktorbezeichner PlayerInputComponentTemplate korregiert

--- SideScroller/src/control/PlayerInputComponentTemplate.java
+++ SideScroller/src/control/PlayerInputComponentTemplate.java
@@ -1,17 +1,17 @@
 package control;
 
-import java.awt.Component;
+import gdw.entityCore.Component;
+import gdw.entityCore.ComponentTemplate;
+
 import java.util.HashMap;
 
-public class PlayerInputComponentTemplate extends ComponentTemplate
-{
+public class PlayerInputComponentTemplate extends ComponentTemplate {
 	private int downKey, jumpKey, leftKey, rightKey, attackKey, specattackKey;
 
 	private float jumpVelocity, runVelocity;
 
-	public PlayerInputComponentManager(HashMap<String, String> params)
-	{
-		super.params = params;
+	public PlayerInputComponentTemplate(HashMap<String, String> params) {
+		super(params);
 
 		downKey = super.getIntParam("DownKey");
 		jumpKey = super.getIntParam("JumpKey");
@@ -24,8 +24,8 @@ public class PlayerInputComponentTemplate extends ComponentTemplate
 		runVelocity = super.getIntParam("RunVelocity");
 	}
 
-	public Component createComponent()
-	{
+	@Override
+	public Component createComponent() {
 		return new PlayerInputComponent(this, downKey, jumpKey, leftKey,
 				rightKey, attackKey, specattackKey);
 		// TODO: Add/Register to PlayerInputComponentManager
