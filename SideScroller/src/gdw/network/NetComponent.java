61140e1a3859407247eb45f050f395d434dc4907
Markus <Markus@Markus-Notebook>
2012-07-24 13:17:27 +0200

    - added sequence id to DeadReckoningNetMessage and NetComponent in order to distinguish new Dead-Reckoning messages from overdue Dead-Reckoning messages - completed serialize/deserialize functions for EntityBusNetMessage

--- SideScroller/src/gdw/network/NetComponent.java
+++ SideScroller/src/gdw/network/NetComponent.java
@@ -7,12 +7,12 @@ import gdw.entityCore.Message;
 public class NetComponent extends Component
 {
 	public static final int COMPONENT_TYPE = 8;
-	
-	private final G
+	private int sequenceID;
 
 	protected NetComponent(ComponentTemplate template)
 	{
 		super(template);
+		sequenceID = 0;
 	}
 
 	@Override
@@ -25,6 +25,14 @@ public class NetComponent extends Component
 	protected void destroy()
 	{
 		super.destroy();
+	}
+	
+	public int getSequenceID() {
+		return sequenceID;
+	}
+	
+	public void setSequenceID(int sequenceID) {
+		this.sequenceID = sequenceID;
 	}
 	
 	public void sendNetworkMessage(Message msg)
