294380fd982abcd3ddabca8dac6c5e8a65ac4944
Michael Kulikow <michael.kulikow@gmail.com>
2012-07-24 17:05:22 +0200

    sendInputToPlayerComponents Methode hinzugefügt

--- SideScroller/src/control/PlayerInputComponentManager.java
+++ SideScroller/src/control/PlayerInputComponentManager.java
@@ -2,34 +2,36 @@ package control;
 
 import java.util.ArrayList;
 
-public class PlayerInputComponentManager
-{
+import org.newdawn.slick.Input;
+
+public class PlayerInputComponentManager {
 	private static PlayerInputComponentManager instance = null;
 
 	private static ArrayList<PlayerInputComponent> playerInpComponents;
 
-	public static PlayerInputComponentManager getInstance()
-	{
-		if (instance == null)
-		{
+	public static PlayerInputComponentManager getInstance() {
+		if (instance == null) {
 			instance = new PlayerInputComponentManager();
 		}
 
 		return instance;
 	}
 
-	private PlayerInputComponentManager()
-	{
+	private PlayerInputComponentManager() {
 		playerInpComponents = new ArrayList<PlayerInputComponent>();
 	}
 
-	public void registerPlayerInputComponent(PlayerInputComponent comp)
-	{
+	public void registerPlayerInputComponent(PlayerInputComponent comp) {
 		playerInpComponents.add(comp);
 	}
 
-	public boolean deregisterPlayerInputComponent(PlayerInputComponent comp)
-	{
+	public boolean deregisterPlayerInputComponent(PlayerInputComponent comp) {
 		return playerInpComponents.remove(comp);
+	}
+
+	public void sendInputToPlayerInputComponents(Input input) {
+		for (PlayerInputComponent currentcomponent : playerInpComponents) {
+			currentcomponent.processingInput(input);
+		}
 	}
 }
