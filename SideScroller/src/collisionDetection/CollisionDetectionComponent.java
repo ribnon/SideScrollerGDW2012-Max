bdd117692bc6f70beeb2071a03faac7a0ca1196a
El Fapo <badrandolf90000@web.de>
2012-07-25 11:44:11 +0200

    QuadTree an den CollisionManager angebunden, aber nicht aktiviert. QuadTree benötigt eine Schnittstelle, die die Kartengröße in Pixel liefert.

--- SideScroller/src/collisionDetection/CollisionDetectionComponent.java
+++ SideScroller/src/collisionDetection/CollisionDetectionComponent.java
@@ -7,48 +7,55 @@ public abstract class CollisionDetectionComponent extends Component
 {
 	public static final int COMPONENT_TYPE = 4;
 	private static int COLLISION_COMPONENT_SUBCLASS_TYPE = -1;
-	
+
 	protected static final int COLLISION_COMPONENT_SUBCLASS_CIRCLE = 0;
 	protected static final int COLLISION_COMPONENT_SUBCLASS_AABOX = 1;
 	protected static final int COLLISION_COMPONENT_SUBCLASS_OOBOX = 2;
-	
+
 	private CollisionQuadTreeRect treeRect;
-	
-	public CollisionDetectionComponent(ComponentTemplate template, int subClassType)
+
+	public CollisionDetectionComponent(ComponentTemplate template,
+			int subClassType)
 	{
 		super(template);
 		COLLISION_COMPONENT_SUBCLASS_TYPE = subClassType;
 		treeRect = null;
 	}
-	
+
 	protected void destroy()
 	{
 	}
-	
+
+	public void tick(float deltaTime)
+	{
+		CollisionDetectionComponentManager.getInstance().detectCollision(this);
+	}
+
 	protected int getSubClassType()
 	{
 		return COLLISION_COMPONENT_SUBCLASS_TYPE;
 	}
-	
+
 	public int getComponentTypeID()
 	{
 		return COMPONENT_TYPE;
 	}
-	
+
 	protected void attachTreeRect(CollisionQuadTreeRect treeRect)
 	{
 		this.treeRect = treeRect;
 	}
-	
+
 	protected CollisionQuadTreeRect unAttachTreeRect()
 	{
 		CollisionQuadTreeRect treeRect = getTreeRect();
 		this.treeRect = null;
 		return treeRect;
 	}
-	
+
 	protected CollisionQuadTreeRect getTreeRect()
 	{
 		return treeRect;
 	}
+
 }
