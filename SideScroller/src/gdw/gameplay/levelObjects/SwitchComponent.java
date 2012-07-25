f6fbbe927263e62c8711c14501dc63dd69fcc806
Michael Kulikow <michael.kulikow@gmail.com>
2012-07-25 23:28:29 +0200

    Konstruktor implementiert

--- SideScroller/src/gdw/gameplay/levelObjects/SwitchComponent.java
+++ SideScroller/src/gdw/gameplay/levelObjects/SwitchComponent.java
@@ -1,7 +1,9 @@
 package gdw.gameplay.levelObjects;
 
 import gdw.entityCore.Component;
+import gdw.entityCore.ComponentTemplate;
 import gdw.entityCore.EntityReference;
+import gdw.entityCore.Message;
 
 public class SwitchComponent extends Component {
 
@@ -9,13 +11,45 @@ public class SwitchComponent extends Component {
 
 	private SwitchType type;
 
-	private enum SwitchType {
+	public enum SwitchType {
 		Hit, Step, Pull
 	}
 
 	private boolean contactFlag, activeFlag;
 
+	// Only by PullComponent
+	private float completedActivation, activationDuration;
+
 	public static final int COMPONENT_TYPE = 16;
+
+	public SwitchComponent(ComponentTemplate template,
+			EntityReference targetEntity, SwitchType type,
+			float completedActivition) {
+		super(template);
+
+		this.targetEntity = targetEntity;
+
+		contactFlag = false;
+		activeFlag = false;
+
+		completedActivation = completedActivition;
+		activationDuration = 0.0f;
+	}
+
+	public SwitchComponent(ComponentTemplate template,
+			EntityReference targetEntity, SwitchType type) {
+		this(template, targetEntity, type, 3.0f);
+	}
+
+	@Override
+	public void tick(float deltaTime) {
+
+	}
+
+	@Override
+	public void onMessage(Message msg) {
+
+	}
 
 	@Override
 	public int getComponentTypeID() {
