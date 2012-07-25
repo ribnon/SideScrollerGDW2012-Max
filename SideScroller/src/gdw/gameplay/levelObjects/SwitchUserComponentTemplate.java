7530f62de16f3694d73a91856274b99165158d1f
Michael Kulikow <michael.kulikow@gmail.com>
2012-07-25 20:45:27 +0200

    Konstruktor implementiert und createComponent Methode überschrieben

--- SideScroller/src/gdw/gameplay/levelObjects/SwitchUserComponentTemplate.java
+++ SideScroller/src/gdw/gameplay/levelObjects/SwitchUserComponentTemplate.java
@@ -3,11 +3,24 @@ package gdw.gameplay.levelObjects;
 import gdw.entityCore.Component;
 import gdw.entityCore.ComponentTemplate;
 
+import java.util.HashMap;
+
 public class SwitchUserComponentTemplate extends ComponentTemplate {
+
+	private boolean hitTrigger;
+
+	public SwitchUserComponentTemplate(HashMap<String, String> params) {
+		super(params);
+
+		int value = super.getIntegerParam("HitTrigger");
+		if (value == 0)
+			hitTrigger = false;
+		else if (value == 1)
+			hitTrigger = true;
+	}
 
 	@Override
 	public Component createComponent() {
-		// TODO Auto-generated method stub
-		return null;
+		return new SwitchUserComponent(this, hitTrigger);
 	}
 }
