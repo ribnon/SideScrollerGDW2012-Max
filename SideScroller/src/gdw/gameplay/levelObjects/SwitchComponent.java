b0295dfc7926cf42b99a51e0caacf0bcb7570c4e
Michael Kulikow <michael.kulikow@gmail.com>
2012-07-25 18:30:56 +0200

    Klassen SwitchComponent und SwitchUserComponent hinzugefügt

---
+++ SideScroller/src/gdw/gameplay/levelObjects/SwitchComponent.java
@@ -0,0 +1,17 @@
+package gdw.gameplay.levelObjects;
+
+import gdw.entityCore.Component;
+
+public class SwitchComponent extends Component {
+
+	private boolean hitTrigger;
+
+	private boolean pullActive;
+
+	@Override
+	public int getComponentTypeID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
