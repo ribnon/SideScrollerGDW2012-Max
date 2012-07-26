a254d84b8b61393dcbb35681657a2019bc680199
Michael Kulikow <michael.kulikow@gmail.com>
2012-07-26 10:40:54 +0200

    Konstruktor und createComponent Methode implementiert

--- SideScroller/src/gdw/gameplay/levelObjects/SwitchComponentTemplate.java
+++ SideScroller/src/gdw/gameplay/levelObjects/SwitchComponentTemplate.java
@@ -5,16 +5,35 @@ import gdw.entityCore.ComponentTemplate;
 import gdw.entityCore.EntityReference;
 import gdw.gameplay.levelObjects.SwitchComponent.SwitchType;
 
+import java.util.HashMap;
+
 public class SwitchComponentTemplate extends ComponentTemplate {
 
-	private EntityReference targentEntity;
+	private EntityReference targetEntity;
 	private SwitchType type;
-	private float completedActivation, activationDuration;
+	private float activationDuration;
+
+	public SwitchComponentTemplate(HashMap<String, String> params) {
+		super(params);
+
+		targetEntity = super.getEntityReferenceParam("TargetEntity");
+		String strtype = super.getStringParam("SwitchType");
+		if (strtype.equals("Hit")) {
+			type = SwitchType.Hit;
+		} else if (strtype.equals("Step")) {
+			type = SwitchType.Step;
+		} else if (strtype.equals("Pull")) {
+			type = SwitchType.Pull;
+		}
+		activationDuration = super.getFloatParam("activationDuration");
+	}
 
 	@Override
 	public Component createComponent() {
-		// TODO Auto-generated method stub
-		return null;
+		if (type == SwitchType.Pull)
+			return new SwitchComponent(this, targetEntity, type,
+					activationDuration);
+		else
+			return new SwitchComponent(this, targetEntity, type);
 	}
-
 }
