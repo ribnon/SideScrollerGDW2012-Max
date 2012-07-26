1b16b39c65b1529200b642e84975d52fb65037f9
max <max@localhost.localdomain>
2012-07-26 15:07:44 +0200

    added basic Level rendering capabilities to graphics.spriteManager

--- SideScroller/src/gdw/graphics/SpriteManager.java
+++ SideScroller/src/gdw/graphics/SpriteManager.java
@@ -1,6 +1,10 @@
 package gdw.graphics;
 
+import gdw.entityCore.Level;
+
 import java.util.LinkedList;
+
+import org.newdawn.slick.tiled.TiledMap;
 
 public class SpriteManager {
 	
@@ -30,8 +34,20 @@ public class SpriteManager {
 	
 	public void render()
 	{
+		TiledMap map = Level.getInstance().getMap();
+		if(map == null) System.out.println("Error: Map not loaded, nullpointer exception imminent :P");
+		int lc = map.getLayerCount();
+		
 		for(int i=0;i<cameras.size();i++)
 		{
+			for(int j = 0; j < lc; ++j)
+			{
+				if(map.getLayerProperty(j, "invisible", "false").equals("true"))
+					continue;
+				map.render((int)(-cameras.get(i).getOwner().getPosX()+0.5f), (int)(-cameras.get(i).getOwner().getPosY()+0.5f), j);
+					
+			}
+			
 			float posX = cameras.get(i).getOwner().getPosX();
 			float posY = cameras.get(i).getOwner().getPosY();
 			
