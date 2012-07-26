b2e5565875dc02c9c68d59469b21e7390973da9e
Michael Kulikow <michael.kulikow@gmail.com>
2012-07-26 17:58:46 +0200

    onMessage und tickMethode modifiziert

--- SideScroller/src/gdw/gameplay/levelObjects/SwitchComponent.java
+++ SideScroller/src/gdw/gameplay/levelObjects/SwitchComponent.java
@@ -2,8 +2,10 @@ package gdw.gameplay.levelObjects;
 
 import gdw.entityCore.Component;
 import gdw.entityCore.ComponentTemplate;
+import gdw.entityCore.EntityManager;
 import gdw.entityCore.EntityReference;
 import gdw.entityCore.Message;
+import collisionDetection.CollisionDetectionMessage;
 
 public class SwitchComponent extends Component {
 
@@ -24,7 +26,7 @@ public class SwitchComponent extends Component {
 
 	public SwitchComponent(ComponentTemplate template,
 			EntityReference targetEntity, SwitchType type,
-			float completedActivition) {
+			float activationDuration) {
 		super(template);
 
 		this.targetEntity = targetEntity;
@@ -32,8 +34,8 @@ public class SwitchComponent extends Component {
 		contactFlag = false;
 		activeFlag = false;
 
-		completedActivation = completedActivition;
-		activationDuration = 0.0f;
+		completedActivation = 0.0f;
+		activationDuration = activationDuration;
 	}
 
 	public SwitchComponent(ComponentTemplate template,
@@ -41,14 +43,69 @@ public class SwitchComponent extends Component {
 		this(template, targetEntity, type, 3.0f);
 	}
 
+	public EntityReference getTargetEntity() {
+		return targetEntity;
+	}
+
+	public void setTargetEntity(EntityReference targetEntity) {
+		this.targetEntity = targetEntity;
+	}
+
+	public SwitchType getType() {
+		return type;
+	}
+
+	public void setType(SwitchType type) {
+		this.type = type;
+	}
+
+	public float getActivationDuration() {
+		return activationDuration;
+	}
+
+	public void setActivationDuration(float activationDuration) {
+		this.activationDuration = activationDuration;
+	}
+
+	public float getCompletedActivation() {
+		return completedActivation;
+	}
+
 	@Override
 	public void tick(float deltaTime) {
+		// Handle for pullswitch
+		if (getType() == SwitchType.Pull) {
 
+			if (!activeFlag && contactFlag) {
+				completedActivation += deltaTime;
+			}
+
+		}
 	}
 
 	@Override
 	public void onMessage(Message msg) {
+		// Handle for pullswitch
+		if (getType() == SwitchType.Pull) {
+			if (msg instanceof CollisionDetectionMessage) {
+				CollisionDetectionMessage tmpmsg = (CollisionDetectionMessage) msg;
 
+				if (!contactFlag && !activeFlag) {
+					int sourceid = targetEntity.getID() != tmpmsg
+							.getIDCandidate1() ? tmpmsg.getIDCandidate1()
+							: tmpmsg.getIDCandidate2();
+
+					SwitchUserComponent swusrcomp = (SwitchUserComponent) EntityManager
+							.getInstance().getEntity(sourceid)
+							.getComponent(SwitchUserComponent.COMPONENT_TYPE);
+
+					if ((swusrcomp != null) && swusrcomp.getpullActive()) {
+						contactFlag = true;
+					}
+				}
+
+			}
+		}
 	}
 
 	@Override
