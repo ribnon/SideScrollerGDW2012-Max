26c5803817e05fff48051bb173154eb8932e7571
Oliver Waidler <waidlero@fh-trier.de>
2012-07-27 23:27:11 +0200

    Minor bugfixes

--- SideScroller/src/gdw/control/PlayerInputComponentTemplate.java
+++ SideScroller/src/gdw/control/PlayerInputComponentTemplate.java
@@ -5,10 +5,13 @@ import gdw.entityCore.ComponentTemplate;
 
 import java.util.HashMap;
 
-public class PlayerInputComponentTemplate extends ComponentTemplate {
+public class PlayerInputComponentTemplate extends ComponentTemplate
+{
 	private int downKey, jumpKey, leftKey, rightKey, attackKey, specattackKey;
 
 	private float jumpVelocity, runVelocity;
+	private long waitingTime;
+	private boolean isUnflippedRight;
 
 	/**
 	 * PlayerInputComponentTemplate constructor which initializes:
@@ -19,29 +22,35 @@ public class PlayerInputComponentTemplate extends ComponentTemplate {
 	 * 
 	 * @param params
 	 */
-	public PlayerInputComponentTemplate(HashMap<String, String> params) {
+	public PlayerInputComponentTemplate(HashMap<String, String> params)
+	{
 		super(params);
 
-		downKey = PlayerInputComponentManager.getKeyValue(super.getStringParam(
-				"DownKey", "DOWN"));
-		jumpKey = PlayerInputComponentManager.getKeyValue(super.getStringParam(
-				"JumpKey", "UP"));
-		leftKey = PlayerInputComponentManager.getKeyValue(super.getStringParam(
-				"LeftKey", "LEFT"));
-		rightKey = PlayerInputComponentManager.getKeyValue(super
-				.getStringParam("RightKey", "RIGHT"));
-		attackKey = PlayerInputComponentManager.getKeyValue(super
-				.getStringParam("AttackKey", "SPACE"));
-		specattackKey = PlayerInputComponentManager.getKeyValue(super
-				.getStringParam("SpecialAttackKey", "RCONTROL"));
+		downKey = PlayerInputComponentManager.getInstance().getKeyValue(
+				super.getStringParam("DownKey", "DOWN"));
+		jumpKey = PlayerInputComponentManager.getInstance().getKeyValue(
+				super.getStringParam("JumpKey", "UP"));
+		leftKey = PlayerInputComponentManager.getInstance().getKeyValue(
+				super.getStringParam("LeftKey", "LEFT"));
+		rightKey = PlayerInputComponentManager.getInstance().getKeyValue(
+				super.getStringParam("RightKey", "RIGHT"));
+		attackKey = PlayerInputComponentManager.getInstance().getKeyValue(
+				super.getStringParam("AttackKey", "SPACE"));
+		specattackKey = PlayerInputComponentManager.getInstance().getKeyValue(
+				super.getStringParam("SpecialAttackKey", "RCONTROL"));
 
 		jumpVelocity = super.getIntegerParam("JumpVelocity", 5);
 		runVelocity = super.getIntegerParam("RunVelocity", 10);
+
+		waitingTime = super.getIntegerParam("waitingTime", 500);
+		isUnflippedRight = super.getIntegerParam("isUnflippedRight", 0) == 1;
 	}
 
 	@Override
-	public Component createComponent() {
+	public Component createComponent()
+	{
 		return new PlayerInputComponent(this, downKey, jumpKey, leftKey,
-				rightKey, attackKey, specattackKey, jumpVelocity, runVelocity);
+				rightKey, attackKey, specattackKey, jumpVelocity, runVelocity,
+				waitingTime, isUnflippedRight);
 	}
 }
