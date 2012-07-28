c838716cfd522cff8faa02a3c6f0bbdee89f7752
Stefan Bodenschatz <stefan_bodenschatz@hotmail.com>
2012-07-28 16:05:36 +0200

    Bugfix EntityCore die Zweite.

--- SideScroller/src/gdw/entityCore/EntityManager.java
+++ SideScroller/src/gdw/entityCore/EntityManager.java
@@ -117,7 +117,7 @@ public class EntityManager {
 	public void loadEntitiesFromLevel(){
 		TiledMap map = Level.getInstance().getMap();
 		int collisionLayerIndex = map.getLayerIndex("Collision");
-		EntityTemplate colBoxTemplate = EntityTemplateManager.getInstance().getEntityTemplate(" --- CollisionTile --- ");
+		EntityTemplate colBoxTemplate = EntityTemplateManager.getInstance().getEntityTemplate(" CollisionTile <internal> ");
 		for(int x=0;x<map.getWidth();++x){
 			for(int y=0;y<map.getHeight();++y){
 				if(map.getTileId(x, y, collisionLayerIndex)==1){
